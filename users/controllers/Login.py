bff6415504a5cd455ee86d3ae0dd271bc73ad2dd
bachrc <8.bachrc@gmail.com>
2019-07-31 10:10:10 +0200

    Le login fonctionne

--- users/controllers/Login.py
+++ users/controllers/Login.py
@@ -1,6 +1,6 @@
 import jwt
 from django.contrib.auth import user_logged_in
-from rest_framework import status
+from rest_framework import status, exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -15,23 +15,21 @@ class Login(APIView):
             email = request.data['email']
             password = request.data['password']
 
-            user = Utilisateur.objects.get(email=email, password=password)
-            if user:
-                try:
-                    payload = jwt_payload_handler(user)
-                    token = jwt.encode(payload, settings.SECRET_KEY)
-                    user_details = {'name': "%s %s" % (
-                        user.first_name, user.last_name), 'token': token}
-                    user_logged_in.send(sender=user.__class__,
-                                        request=request, user=user)
-                    return Response(user_details, status=status.HTTP_200_OK)
+            user = Utilisateur.objects.get(email=email)
+            if not user.check_password(password):
+                raise Utilisateur.DoesNotExist
 
-                except Exception as e:
-                    raise e
-            else:
-                res = {
-                    'error': 'can not authenticate with the given credentials or the account has been deactivated'}
-                return Response(res, status=status.HTTP_403_FORBIDDEN)
+            payload = jwt_payload_handler(user)
+            token = jwt.encode(payload, settings.SECRET_KEY)
+            user_details = {'name': "%s %s" % (
+                user.first_name, user.last_name), 'token': token}
+            user_logged_in.send(sender=user.__class__,
+                                request=request, user=user)
+            return Response(user_details, status=status.HTTP_200_OK)
+
+        except Utilisateur.DoesNotExist:
+            raise exceptions.AuthenticationFailed('No such user')
+
         except KeyError:
             res = {'error': 'please provide a email and a password'}
             return Response(res)
