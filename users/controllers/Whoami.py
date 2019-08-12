4c52e783ae088b6bef401da5219cbb736f03f161
bachrc <8.bachrc@gmail.com>
2019-08-13 00:11:41 +0200

    Whoami fonctionnel

--- users/controllers/Whoami.py
+++ users/controllers/Whoami.py
@@ -2,9 +2,17 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from users import dto
+from users.dto.Whoami import Whoami as WhoamiDTO
+from users.models import Utilisateur
+
 
 class Whoami(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
-        return Response("eh oui")
+        user: Utilisateur = Utilisateur.objects.get(id=request.user.id)
+
+        dto = WhoamiDTO(id_utilisateur=user.id, prenom=user.first_name, nom=user.last_name)
+
+        return Response(dto.data)
