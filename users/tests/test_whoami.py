0ecb5fc8993a0c4214036558df3e06e596a11bea
bachrc <8.bachrc@gmail.com>
2019-08-12 20:44:52 +0200

    Création du test pour le Whoami

---
+++ users/tests/test_whoami.py
@@ -0,0 +1,45 @@
+import jwt
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from battledevseverus import settings
+from users.models import Utilisateur
+
+ID_UTILISATEUR_1 = 1
+PRENOM_UTILISATEUR_1 = "Pierre"
+NOM_UTILISATEUR_1 = "Martinet"
+MAIL_UTILISATEUR_1 = "p.martinet@live.fr"
+PASS_UTILISATEUR_1 = "intraitable"
+
+
+class WhoamiTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.utilisateur1 = Utilisateur.objects.create_user(id=ID_UTILISATEUR_1,
+                                                           last_name=NOM_UTILISATEUR_1,
+                                                           first_name=PRENOM_UTILISATEUR_1,
+                                                           email=MAIL_UTILISATEUR_1,
+                                                           password=PASS_UTILISATEUR_1)
+
+    def test_should_return_correct_info_for_whoami(self):
+        url_login = reverse('login')
+        response_login = self.client.post(url_login, {
+            "email": MAIL_UTILISATEUR_1,
+            "password": PASS_UTILISATEUR_1
+        }, format='json')
+
+        self.assertEqual(response_login.status_code, status.HTTP_200_OK)
+        self.assertTrue("token" in response_login.data)
+
+        url_whoami = reverse("whoami")
+        auth_headers = {
+            'HTTP_AUTHORIZATION': 'Bearer ' + (response_login.data["token"].decode()),
+        }
+
+        response_whoami = self.client.get(url_whoami, **auth_headers)
+        print(response_whoami)
+        self.assertEqual(response_whoami.status_code, status.HTTP_200_OK)
+        self.assertEqual(response_whoami.data["id_utilisateur"], ID_UTILISATEUR_1)
+        self.assertEqual(response_whoami.data["prenom"], PRENOM_UTILISATEUR_1)
+        self.assertEqual(response_whoami.data["nom"], NOM_UTILISATEUR_1)
