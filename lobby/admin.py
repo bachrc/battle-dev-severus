7bc5a89e382e8d428b395f4679af486d4c2788d6
bachrc <8.bachrc@gmail.com>
2019-08-26 23:49:31 +0200

    Administration des battle dev

--- lobby/admin.py
+++ lobby/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from lobby.models import BattleDev
 from .models import Probleme, Question
 
 
@@ -11,6 +12,11 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ('intitule',)
 
 
+class BattleDevAdmin(admin.ModelAdmin):
+    list_display = ('nom',)
+
+
 # Register your models here.
 admin.site.register(Probleme, ProblemeAdmin)
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(BattleDev, BattleDevAdmin)
